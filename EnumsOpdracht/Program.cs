1f03573ca8b1cdcffa025dc066fbf740535d4c90
GaelMoney <25393@ma-web.nl>
2022-05-23 13:09:06 +0200

    deleted old files added EnumsOpdrcht and added elementSkills inside Enums

---
+++ EnumsOpdracht/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnumsOpdracht
+{
+    enum ElementSkills
+    {
+        FireBall = 1,
+        GreatFireBall = 2,
+        Waterball = 3,
+        WaterTorrent = 4,
+                       
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+           
+      
+            
+            
+
+        }
+    }
+}
