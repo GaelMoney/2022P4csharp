7728c39ad4541643e431d469e936f3409dce8604
GaelMoney <25393@ma-web.nl>
2022-06-28 15:18:38 +0200

    added last files

---
+++ StringOpdracht/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace StringOpdracht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringText = File.ReadAllText(@"C:\Users\gaelg\Documents\Periode 4 Eerste jaar\2022P4csharp\StringOpdracht\stringsplit.txt");
+            string[] text = stringText.Split(':', ',');
+
+            foreach (string tekst in text)
+            {
+                Console.WriteLine(tekst);
+            }
+        }
+    }
+}
