0742423aa699f1b1833d58f536eeef0a029faef1
GaelMoney <25393@ma-web.nl>
2022-06-28 15:00:25 +0200

    added new files and changed some files

---
+++ FileIOOpdracht/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+namespace FileIOOpdracht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Text = File.ReadAllLines(@"C:\Users\gaelg\Documents\Periode 4 Eerste jaar\2022P4csharp\FileIOOpdracht\Text.txt");
+
+            foreach (string Line in Text)
+                Console.WriteLine(Line);
+        }
+    }
+}
